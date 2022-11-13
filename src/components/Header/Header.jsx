d9ac86990fc4a24a9dc612e8742339173354939d
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-13 08:29:44 +0100

    feat: added header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,32 @@
+import './Header.css';
+import HeroImg from '../../assets/hero-img.png';
+
+const Header = () => {
+  return (
+    <header className="container header">
+      <div className="hero-text-box">
+        <h1 className="heading-primary">
+          Rent a <span>Place</span> away from <span>Home</span> in the{' '}
+          <span>Metaverse</span>
+        </h1>
+
+        <p className="heading-text">
+          we provide you access to luxury and affordable houses in the
+          metaverse, get a chance to turn your imagination to reality at your
+          comfort zone
+        </p>
+
+        <form action="">
+          <input type="text" placeholder="Search for location" />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+
+      <div className="hero-img-box">
+        <img src={HeroImg} alt="metabnb demo images" className="hero-img" />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
