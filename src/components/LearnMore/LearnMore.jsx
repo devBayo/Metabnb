40731e343cacae99f235b0d54102250c9044ef5f
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-15 21:00:14 +0100

    feat: added section

---
+++ src/components/LearnMore/LearnMore.jsx
@@ -0,0 +1,27 @@
+import './LearnMore.css';
+import Frame from '../../assets/frame.png';
+
+const LearnMore = () => {
+  return (
+    <section className="learn-more-section">
+      <div className="container learn-more-container">
+        <div className="text-box">
+          <h2 className="heading-secondary">Metabnb NFTs</h2>
+          <p className="text-desc">
+            Discover our NFT gift cards collection. Loyal customers gets amazing
+            gift cards which are traded as NFTs. These NFTs gives our cutomer
+            access to loads of our exclusive services.
+          </p>
+          <button className="btn-learn-more">Learn more</button>
+        </div>
+        <img
+          src={Frame}
+          className="learn-more-img"
+          alt="Three visually appealing nfts"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default LearnMore;
