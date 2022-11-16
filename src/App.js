2705e617ec66bec386726e4f7d0b4cdcb5ce1172
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-16 10:21:17 +0100

    feat: added state to toggle

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Nav from './components/Nav/Nav';
 import Home from './pages/Home';
@@ -6,11 +6,31 @@ import Footer from './components/Footer/Footer';
 import Places from './pages/Places';
 
 function App() {
+  const [showModal, setShowModal] = useState(false);
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setNavIsOpen(prevVal => !prevVal);
+  };
+
+  const toggleModal = () => {
+    setNavIsOpen(false);
+    setShowModal(prevVal => !prevVal);
+  };
+
   return (
     <React.Fragment>
-      <Nav />
+      <Nav
+        navIsOpen={navIsOpen}
+        toggleNav={toggleNav}
+        showModal={showModal}
+        onClick={toggleModal}
+      />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={<Home showModal={showModal} onClick={toggleModal} />}
+        />
         <Route path="/place-to-stay" element={<Places />} />
       </Routes>
       <Footer />
