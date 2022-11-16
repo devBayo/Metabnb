ab976bdd9d26b87f02e7cd8972199318aa0049f7
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-16 10:26:44 +0100

    added toggle feat: feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,16 +2,16 @@ import Header from '../components/Header/Header';
 import Sponsors from '../components/Sponsors/Sponsors';
 import Showcase from '../components/Showcase/Showcase';
 import LearnMore from '../components/LearnMore/LearnMore';
-// import Modal from '../components/Modal/Modal';
+import Modal from '../components/Modal/Modal';
 
-const Home = () => {
+const Home = props => {
   return (
     <main>
       <Header />
       <Sponsors />
       <Showcase />
       <LearnMore />
-      {/* <Modal /> */}
+      {props.showModal && <Modal onClick={props.onClick} />}
     </main>
   );
 };
