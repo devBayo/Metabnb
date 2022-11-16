13407ae4c9398cb6884be5e709973d1e4cc3cbdf
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-16 03:52:03 +0100

    feat: exported nfts

--- src/components/Showcase/Showcase.jsx
+++ src/components/Showcase/Showcase.jsx
@@ -9,7 +9,8 @@ import nft7 from '../../assets/nft7.png';
 import nft8 from '../../assets/nft8.png';
 import Nfts from '../Nfts/Nfts';
 
-const nfts = [nft1, nft2, nft3, nft4, nft5, nft6, nft7, nft8];
+export const nfts = [nft1, nft2, nft3, nft4, nft5, nft6, nft7, nft8];
+
 const Showcase = () => {
   return (
     <section className="container showcase-section">
