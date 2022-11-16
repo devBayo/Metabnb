4e91172923e1eeddc217372742688bf190f2b4d0
hashvector <ibrahimabdulqoyyum@gmail.com>
2022-11-16 10:25:39 +0100

    chore: ''

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -21,21 +21,31 @@ const links = [
   },
 ];
 
-const Nav = () => {
+const Nav = props => {
   return (
-    <nav className="container nav">
+    <nav className={`container nav ${props.navIsOpen ? 'nav-open' : ''}`}>
       <img src={NavLogo} alt="Metabnb-Logo" className="nav-logo" />
       <ul className="nav-links">
         {links.map((link, i) => (
           <li key={i} className="nav-item">
-            <Link to={link.href} className="nav-link">
+            <Link onClick={props.toggleNav} to={link.href} className="nav-link">
               {link.text}
             </Link>
           </li>
         ))}
+
+        <button onClick={props.onClick} className="btn btn-cta btn-cta__alt">
+          Connect wallet
+        </button>
       </ul>
 
-      <button className="btn btn-cta">Connect wallet</button>
+      <button onClick={props.onClick} className="btn btn-cta btn-cta__main">
+        Connect wallet
+      </button>
+      <button onClick={props.toggleNav} className="btn-mobile-nav">
+        {!props.navIsOpen && <ion-icon name="menu-outline"></ion-icon>}
+        {props.navIsOpen && <ion-icon name="close-outline"></ion-icon>}
+      </button>
     </nav>
   );
 };
